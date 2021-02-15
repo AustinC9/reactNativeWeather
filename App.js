f99d0294614ab9d40c6a5f73ac4d7c4c4e1feb71
AustinC9 <austin.carson4@gmail.com>
2021-02-15 11:29:24 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.searchBar}>
+        <View style={styles.searchInput}>
+          {/* Icon */}
+          <FontAwesome
+            name="search-location"
+            color="#000"
+            style={styles.icon}
+          ></FontAwesome>
+          {/* Textfield */}
+          <TextInput
+            style={styles.input}
+            placeholder="Search your city name"
+          ></TextInput>
+          {/* Icon */}
+          <FontAwesome
+            name="microphone"
+            color="#000"
+            style={styles.icon}
+          ></FontAwesome>
+        </View>
+        <View style={styles.Tabs}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
+  searchBar: {
+    backgroundColor: "#1976d2",
+    height: 100,
+    width: "100%",
+  },
+  searchInput: {
+    backgroundColor: "#fff",
+    height: 30,
+    width: "90%",
+    margin: 20,
+    padding: 5,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  Tabs: {},
+  input: {
+    flexGrow: 1,
+    marginLeft:10,
+    marginRight:10,
+    fontSize:15,
+    border:'none',
+  },
+  icon:{
+    width:25,
+    height:25,
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+  }
 });
